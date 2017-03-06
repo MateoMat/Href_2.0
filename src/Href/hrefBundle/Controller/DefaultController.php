f3a9c2e3f9b46337f6346d50db460d64e56cfd8a
MateoMat <m.maslank@interia.pl>
2017-03-06 12:00:11 +0100

    Update

--- src/Href/hrefBundle/Controller/DefaultController.php
+++ src/Href/hrefBundle/Controller/DefaultController.php
@@ -9,13 +9,30 @@ class DefaultController extends Controller
 
     public function indexAction()
     {
-        $users=$this->getDoctrine()->getRepository('hrefBundle:User');
-        $users=$users->findAll();
+        $users = $this->getDoctrine()->getRepository('hrefBundle:User');
+        $users = $users->findAll();
 
-        $structure=$this->getDoctrine()->getRepository('hrefBundle:Structure');
-        $structures=$structure->findAll();
+        $em = $this->getDoctrine()->getRepository('hrefBundle:Structure');
+        $structures = $em->findAll();
+
+        $gm = $em->findByLevel(1);
+        $bud = $em->findByLevel(2);
+        $ff = $em->findByLevel(3);
+        $dm = $em->findByLevel(4);
+        $rep = $em->findByLevel(5);
+
+        $level4Count=count($dm);
 
 
-        return $this->render('hrefBundle:Default:index.html.twig',array('users'=>$users,'structures'=>$structures));
+        return $this->render('hrefBundle:Default:index.html.twig', array(
+            'users' => $users,
+            'structures' => $structures,
+            'gm' => $gm,
+            'bud' => $bud,
+            'ff' => $ff,
+            'dm' => $dm,
+            'rep' => $rep,
+            'dmCount'=>$level4Count
+        ));
     }
 }
